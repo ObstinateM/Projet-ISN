657126a8d7dbe48f0f475ac52042fb6b7609d3f2
Obstinate <mathis.beauville@gmail.com>
2020-03-22 16:31:54 +0100

    Adding the Review model

--- card/admin.py
+++ card/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Cartes
+from .models import Cartes, Review
 
 # Register your models here.
 
 
 admin.site.register(Cartes)
+admin.site.register(Review)
--- card/models.py
+++ card/models.py
@@ -12,3 +12,14 @@ class Cartes(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Review(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    card_id = models.ForeignKey(Cartes, on_delete=models.CASCADE, default=None) # Ajouter QUE Cartes.id
+    review_date = models.DateField(auto_now=True)
+    review_level = models.IntegerField()
+
+    def __str__(self):
+        return self.title

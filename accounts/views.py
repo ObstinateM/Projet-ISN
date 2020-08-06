2496f88d4b09b20a0f986fafac839e36c91b2e60
Mathis Beauville <mathis.beauville@gmail.com>
2020-08-06 15:52:55 +0200

    Check if user is authentificated before going on the login/register page

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 
 def register(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('home')
     if request.method == "POST":
         form = RegisterForm(request.POST, label_suffix='')
         if form.is_valid():
             user = form.save()
             login(request, user)
+            return redirect('home')
     else:
         form = RegisterForm(label_suffix='')
     return render(request, 'accounts/register.html', {"form":form})
 
 
 def login_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('home')
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST, label_suffix='')
         if form.is_valid():
             user = form.get_user()
             login(request, user)
+            return redirect('home')
     else:
         form = AuthenticationForm(label_suffix='') 
     return render(request, 'accounts/login.html', {'form':form})
--- main/views.py
+++ main/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/home.html', {})
+    user = request.user
+    context = {'user': user}
+    return render(request, 'main/home.html', context)
